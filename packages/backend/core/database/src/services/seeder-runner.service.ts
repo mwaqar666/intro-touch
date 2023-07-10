5fd502272625b439bb0c74341671394d6ab814eb
Muhammad Waqar <mwaqar666@yahoo.com>
2023-07-10 21:24:26 +0500

    Merge pull request #4
    * Truncate db before every seed

--- packages/backend/core/database/src/services/seeder-runner.service.ts
+++ packages/backend/core/database/src/services/seeder-runner.service.ts
@@ -1,15 +1,19 @@
 import { Inject } from "iocc";
+import type { Sequelize } from "sequelize-typescript";
 import { DbTokenConst } from "@/backend-core/database/const";
-import type { IDbManager } from "@/backend-core/database/interface/db";
+import type { IDbConnector, IDbManager } from "@/backend-core/database/interface/db";
 import type { ISeeder, ISeederRunner } from "@/backend-core/database/interface/seeder";
 
 export class SeederRunnerService implements ISeederRunner {
 	public constructor(
 		// Dependencies
 		@Inject(DbTokenConst.DbManagerToken) private readonly dbManager: IDbManager,
+		@Inject(DbTokenConst.DbConnectorToken) private readonly dbConnector: IDbConnector<Sequelize>,
 	) {}
 
 	public async runSeeders(): Promise<Array<string>> {
+		await this.truncateDatabase();
+
 		const dbSeeders: Array<ISeeder> = this.gatherDatabaseSeeders();
 		const seedersRan: Array<string> = [];
 
@@ -20,6 +24,16 @@ export class SeederRunnerService implements ISeederRunner {
 		}
 
 		return seedersRan;
+	}
+
+	private async truncateDatabase(): Promise<void> {
+		const sequelize: Sequelize = this.dbConnector.getDatabaseConnection();
+
+		await sequelize.truncate({
+			cascade: true,
+			restartIdentity: true,
+			force: true,
+		});
 	}
 
 	private gatherDatabaseSeeders(): Array<ISeeder> {
