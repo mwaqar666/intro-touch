979720fb92fc0ac525d0d896a07003dbf8139471
Muhammad Waqar <mwaqar666@yahoo.com>
2023-07-24 18:52:46 +0500

    Refactored platform seeder

--- packages/backend/platform/src/db/seeders/platforms.seeder.ts
+++ packages/backend/platform/src/db/seeders/platforms.seeder.ts
@@ -1,9 +1,21 @@
 import { DbTokenConst } from "@/backend-core/database/const";
 import type { ITransactionManager } from "@/backend-core/database/interface";
 import type { ISeeder } from "@/backend-core/database/interface/seeder";
-import type { ITransactionStore } from "@/backend-core/database/types";
+import type { IEntityTableColumnProperties, ITransactionStore } from "@/backend-core/database/types";
+import type { Optional } from "@/stacks/types";
 import { Inject } from "iocc";
+import type { PlatformCategoryEntity, PlatformEntity } from "@/backend/platform/db/entities";
 import { PlatformCategoryRepository, PlatformRepository } from "@/backend/platform/db/repositories";
+
+interface IPlatform {
+	platformName: string;
+	platformIcon: string;
+}
+
+interface IPlatformCategory {
+	platformCategoryName: string;
+	platformCategoryPlatforms: Array<IPlatform>;
+}
 
 export class PlatformsSeeder implements ISeeder {
 	public timestamp = 1689226980338;
@@ -16,56 +28,88 @@ export class PlatformsSeeder implements ISeeder {
 	) {}
 
 	public async seed(): Promise<void> {
+		const platformCategorySeedData: Array<IPlatformCategory> = [
+			{
+				platformCategoryName: "Get in touch",
+				platformCategoryPlatforms: [
+					{
+						platformName: "Call",
+						platformIcon: "https://intro-touch-attachment.s3.us-east-2.amazonaws.com/common/phone.png",
+					},
+					{
+						platformName: "Message",
+						platformIcon: "https://intro-touch-attachment.s3.us-east-2.amazonaws.com/common/messages.png",
+					},
+					{
+						platformName: "Calendly",
+						platformIcon: "https://intro-touch-attachment.s3.us-east-2.amazonaws.com/common/calendly.png",
+					},
+					{
+						platformName: "Maps",
+						platformIcon: "https://intro-touch-attachment.s3.us-east-2.amazonaws.com/common/google+map.png",
+					},
+					{
+						platformName: "Mail",
+						platformIcon: "https://intro-touch-attachment.s3.us-east-2.amazonaws.com/common/apple_mail.png",
+					},
+				],
+			},
+			{
+				platformCategoryName: "Payment links",
+				platformCategoryPlatforms: [
+					{
+						platformName: "Venmo",
+						platformIcon: "https://intro-touch-attachment.s3.us-east-2.amazonaws.com/common/venmo.png",
+					},
+					{
+						platformName: "Paypal",
+						platformIcon: "https://intro-touch-attachment.s3.us-east-2.amazonaws.com/common/paypal.png",
+					},
+					{
+						platformName: "CashApp",
+						platformIcon: "https://intro-touch-attachment.s3.us-east-2.amazonaws.com/common/cashapp.png",
+					},
+				],
+			},
+			{
+				platformCategoryName: "Social links",
+				platformCategoryPlatforms: [],
+			},
+		];
+
 		await this.transactionManager.executeTransaction({
 			operation: async ({ transaction }: ITransactionStore): Promise<void> => {
-				await this.platformCategoryRepository.createMany({
-					valuesToCreate: [{ platformCategoryName: "GET_IN_TOUCH" }, { platformCategoryName: "PAYMENT_LINKS" }, { platformCategoryName: "SOCIAL_LINKS" }],
+				const platformCategories: Array<PlatformCategoryEntity> = await this.platformCategoryRepository.createMany({
+					valuesToCreate: platformCategorySeedData.map((platformCategorySeedDatum: IPlatformCategory): Partial<IEntityTableColumnProperties<PlatformCategoryEntity>> => {
+						return {
+							platformCategoryName: platformCategorySeedDatum.platformCategoryName,
+						};
+					}),
 					transaction,
 				});
 
+				const platformSeedData: Array<Partial<IEntityTableColumnProperties<PlatformEntity>>> = [];
+
+				for (const platformCategory of platformCategories) {
+					const platformCategorySeedDatum: Optional<IPlatformCategory> = platformCategorySeedData.find((platformCategorySeedDatum: IPlatformCategory): boolean => {
+						return platformCategorySeedDatum.platformCategoryName === platformCategory.platformCategoryName;
+					});
+
+					if (!platformCategorySeedDatum) continue;
+
+					platformSeedData.push(
+						...platformCategorySeedDatum.platformCategoryPlatforms.map((platformSeedDatum: IPlatform): Partial<IEntityTableColumnProperties<PlatformEntity>> => {
+							return {
+								platformPlatformCategoryId: platformCategory.platformCategoryId,
+								platformName: platformSeedDatum.platformName,
+								platformIcon: platformSeedDatum.platformIcon,
+							};
+						}),
+					);
+				}
+
 				await this.platformRepository.createMany({
-					valuesToCreate: [
-						{
-							platformPlatformCategoryId: 1,
-							platformName: "Call",
-							platformIcon: "https://intro-touch-attachment.s3.us-east-2.amazonaws.com/common/phone.png",
-						},
-						{
-							platformPlatformCategoryId: 1,
-							platformName: "Message",
-							platformIcon: "https://intro-touch-attachment.s3.us-east-2.amazonaws.com/common/messages.png",
-						},
-						{
-							platformPlatformCategoryId: 1,
-							platformName: "Calendly",
-							platformIcon: "https://intro-touch-attachment.s3.us-east-2.amazonaws.com/common/calendly.png",
-						},
-						{
-							platformPlatformCategoryId: 1,
-							platformName: "Maps",
-							platformIcon: "https://intro-touch-attachment.s3.us-east-2.amazonaws.com/common/google+map.png",
-						},
-						{
-							platformPlatformCategoryId: 1,
-							platformName: "Mail",
-							platformIcon: "https://intro-touch-attachment.s3.us-east-2.amazonaws.com/common/apple_mail.png",
-						},
-						{
-							platformPlatformCategoryId: 2,
-							platformName: "Venmo",
-							platformIcon: "https://intro-touch-attachment.s3.us-east-2.amazonaws.com/common/venmo.png",
-						},
-						{
-							platformPlatformCategoryId: 2,
-							platformName: "Paypal",
-							platformIcon: "https://intro-touch-attachment.s3.us-east-2.amazonaws.com/common/paypal.png",
-						},
-						{
-							platformPlatformCategoryId: 2,
-							platformName: "CashApp",
-							platformIcon: "https://intro-touch-attachment.s3.us-east-2.amazonaws.com/common/cashapp.png",
-						},
-					],
+					valuesToCreate: platformSeedData,
 					transaction,
 				});
 			},
