ac970f8148c918f1e0afcc657596255ce5cd08a3
Muhammad Waqar <mwaqar666@yahoo.com>
2024-03-20 15:40:52 +0100

    Remove register email

--- packages/backend/core/authentication/src/services/sign-up.service.ts
+++ packages/backend/core/authentication/src/services/sign-up.service.ts
@@ -2,27 +2,27 @@ import type { UserEntity } from "@/backend/user/db/entities";
 import { UserAuthService } from "@/backend/user/services";
 import type { IFindOrCreateUserProps } from "@/backend/user/types";
 import { Inject } from "iocc";
-import { VerificationTokenService } from "@/backend-core/authentication/dal";
-import type { VerificationTokenEntity } from "@/backend-core/authentication/db/entities";
 import type { RegisterRequestDto } from "@/backend-core/authentication/dto/register";
-import { EmailUtilService } from "@/backend-core/authentication/utils";
 
 export class SignUpService {
 	public constructor(
 		// Dependencies
 		@Inject(UserAuthService) private readonly userAuthService: UserAuthService,
-		@Inject(EmailUtilService) private readonly emailUtilService: EmailUtilService,
-		@Inject(VerificationTokenService) private readonly verificationTokenService: VerificationTokenService,
+		// @Inject(EmailUtilService) private readonly emailUtilService: EmailUtilService,
+		// @Inject(VerificationTokenService) private readonly verificationTokenService: VerificationTokenService,
 	) {}
 
 	public async basicRegister(registerRequestDto: RegisterRequestDto): Promise<boolean> {
 		const findOrCreateUserProps: IFindOrCreateUserProps = { ...registerRequestDto, userParentId: 1 };
 
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-expect-error
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 		const user: UserEntity = await this.userAuthService.createNewUserWithProfile(findOrCreateUserProps);
 
-		const verificationToken: VerificationTokenEntity = await this.verificationTokenService.createEmailVerificationToken(user);
-
-		await this.emailUtilService.sendAccountVerificationEmailToUser(user, verificationToken);
+		// const verificationToken: VerificationTokenEntity = await this.verificationTokenService.createEmailVerificationToken(user);
+		//
+		// await this.emailUtilService.sendAccountVerificationEmailToUser(user, verificationToken);
 
 		return true;
 	}
