96389d7d0cf19f737bd254f9026829ba1f334578
Muhammad Waqar <mwaqar666@yahoo.com>
2024-06-06 18:17:55 +0200

    Seeder fix

--- packages/backend/user/src/db/seeders/users.seeder.ts
+++ packages/backend/user/src/db/seeders/users.seeder.ts
@@ -3,6 +3,7 @@ import type { ITransactionManager } from "@/backend-core/database/interface";
 import type { ISeeder } from "@/backend-core/database/interface/seeder";
 import type { ITransactionStore } from "@/backend-core/database/types";
 import { Inject } from "iocc";
+import type { UserEntity } from "@/backend/user/db/entities";
 import { UserRepository } from "@/backend/user/db/repositories";
 
 export class UsersSeeder implements ISeeder {
@@ -18,24 +19,26 @@ export class UsersSeeder implements ISeeder {
 	public async seed(): Promise<void> {
 		await this.transactionManager.executeTransaction({
 			operation: async ({ transaction }: ITransactionStore): Promise<void> => {
-				await this.userRepository.createMany({
-					valuesToCreate: [
-						{
-							userFirstName: "Muhammad",
-							userLastName: "Waqar",
-							userEmail: "muhammadwaqar666@gmail.com",
-							userPassword: "password",
-							userPicture: "",
-						},
-						{
-							userParentId: 1,
-							userFirstName: "Nabeel",
-							userLastName: "Baig",
-							userEmail: "mathswithnabeel@gmail.com",
-							userPassword: "password",
-							userPicture: "",
-						},
-					],
+				const userEntity: UserEntity = await this.userRepository.createOne({
+					valuesToCreate: {
+						userFirstName: "Muhammad",
+						userLastName: "Waqar",
+						userEmail: "muhammadwaqar666@gmail.com",
+						userPassword: "password",
+						userPicture: "",
+					},
+					transaction,
+				});
+
+				await this.userRepository.createOne({
+					valuesToCreate: {
+						userParentId: userEntity.userParentId,
+						userFirstName: "Nabeel",
+						userLastName: "Baig",
+						userEmail: "mathswithnabeel@gmail.com",
+						userPassword: "password",
+						userPicture: "",
+					},
 					transaction,
 				});
 			},
